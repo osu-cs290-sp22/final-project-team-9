d63626c348b593c548d76e1a52ab8fdf5a2672d8
jim-landers <36310621+jim-landers@users.noreply.github.com>
2022-05-15 20:08:04 -0700

    Setup dotenv for Spotify API keys
    Not sure about the variable names, may change later.
    
    Keys can be accessed with "process.env.VAR_NAME"

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const fs   = require('fs');
 
+
+require('dotenv').config();
+
 // $PORT, otherwise default 8000
 const PORT = process.env.PORT || 8000;
 
-const BOOTSTRAP_JS  = fs.readFileSync('node_modules/bootstrap/dist/js/bootstrap.min.js')
-const BOOTSTRAP_CSS = fs.readFileSync('node_modules/bootstrap/dist/css/bootstrap.min.css')
+const BOOTSTRAP_JS  = fs.readFileSync('node_modules/bootstrap/dist/js/bootstrap.min.js');
+const BOOTSTRAP_CSS = fs.readFileSync('node_modules/bootstrap/dist/css/bootstrap.min.css');
 var   INDEX         = fs.readFileSync('public/index.html');
 
 
@@ -18,11 +21,11 @@ const server = http.createServer(function (req, res) {
     }
     else if (req.url == ('/' + BOOTSTRAP_JS)) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(BOOTSTRAP_JS)
+        res.write(BOOTSTRAP_JS);
     }
     else if (req.url == ('/' + BOOTSTRAP_CSS)) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(BOOTSTRAP_CSS)
+        res.write(BOOTSTRAP_CSS);
     }
 
 
