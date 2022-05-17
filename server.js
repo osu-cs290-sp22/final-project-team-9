c2aa98486c00c22e02a8e409b2bb28139abd7f53
Carter Roeser <carter@cdgtech.one>
2022-05-16 19:20:25 -0700

    Express Server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const url  = require('url');
-const fs   = require('fs');
+const url = require('url');
+const fs = require('fs');
 require('dotenv').config();
 
 const spotifyReq = require('./spotifyrequest.js');
@@ -11,37 +11,33 @@ const AUTH_TOKEN = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SE
 const PORT = process.env.PORT || 8000;
 
 const BOOTSTRAP_DIR = 'node_modules/bootstrap/dist/';
-const BOOTSTRAP_JS  = fs.readFileSync(BOOTSTRAP_DIR + 'js/bootstrap.min.js');
+const BOOTSTRAP_JS = fs.readFileSync(BOOTSTRAP_DIR + 'js/bootstrap.min.js');
 const BOOTSTRAP_CSS = fs.readFileSync(BOOTSTRAP_DIR + 'css/bootstrap.min.css');
-var   INDEX         = fs.readFileSync('public/index.html');
+var INDEX = fs.readFileSync('public/index.html');
 
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer(function(req, res) {
     res.statusCode = 404;
 
-    var route  = new URL(req.url, process.env.CALLBACK_URL);
+    var route = new URL(req.url, process.env.CALLBACK_URL);
     var params = route.searchParams;
 
     console.log(params);
-    
+
     if (route.pathname == '/' || route.pathname == '/index.html') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(INDEX);
-    }
-    else if (route.pathname == ('/' + BOOTSTRAP_DIR + 'js/bootstrap.min.js')) {
-        res.writeHead(200, {'Content-Type': 'text/js'});
+    } else if (route.pathname == ('/assets/js/bootstrap.min.js')) {
+        res.writeHead(200, { 'Content-Type': 'text/js' });
         res.write(BOOTSTRAP_JS);
-    }
-    else if (route.pathname == ('/' + BOOTSTRAP_DIR + 'css/bootstrap.min.css')) {
-        res.writeHead(200, {'Content-Type': 'text/css'});
+    } else if (route.pathname == ('/assets/css/bootstrap.min.css')) {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
         res.write(BOOTSTRAP_CSS);
-    }
-    else if (route.pathname == '/auth') {
+    } else if (route.pathname == '/auth') {
         // let spotifyLoginURL = spotifyReq.GetAuthURL(process.env.CLIENT_ID, ("http://" + req.headers.host + '/callback'));
         let spotifyLoginURL = spotifyReq.GetAuthURL(process.env.CLIENT_ID, process.env.CALLBACK_URL);
-        res.writeHead(307, {'Location': spotifyLoginURL});
-    }
-    else if (route.pathname == '/callback') {
+        res.writeHead(307, { 'Location': spotifyLoginURL });
+    } else if (route.pathname == '/callback') {
         console.log(" * Callback recieved")
 
         payload = {
@@ -53,9 +49,9 @@ const server = http.createServer(function (req, res) {
         };
         // console.log(payload);
         var token = spotifyReq.GetOAuthToken(payload);
-        
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(INDEX);
     }
 
@@ -63,8 +59,6 @@ const server = http.createServer(function (req, res) {
     res.end();
 })
 
-server.listen(PORT, function () {
+server.listen(PORT, function() {
     console.log("== Server is listening on port " + PORT);
 })
-
-
