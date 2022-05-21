c19a37f6ca47dd443b7cae48b382ed1085613cc2
Carter Roeser <carter@cdgtech.one>
2022-05-20 20:23:14 -0700

    Diagonal Load Animation

--- public/index.js
+++ public/index.js
@@ -18,7 +18,55 @@ window.onload = function() {
     var buttons = document.getElementsByClassName("item"),
         len = buttons !== null ? buttons.length : 0,
         i = 0;
-    for(i; i < len; i++) {
-        buttons[i].className += " item-transition"; 
+    for (i; i < len; i++) {
+        buttons[i].className += " item-transition";
+    }
+}
+
+function animateFlex(elClass, wrapper) {
+    // Store elements to variables
+    var innerEls = document.getElementsByClassName(elClass);
+    var outerEl = document.getElementById(wrapper);
+
+    // Compute visible height of inner element
+    var styles = window.getComputedStyle(innerEls[0]);
+    var margin = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom']);
+
+    // Compute rows and columns in grid
+    var row = Math.floor(outerEl.offsetHeight / Math.ceil(innerEls[0].offsetHeight + margin))
+    var col = Math.ceil(innerEls.length / row)
+
+    // Don't animate if Array is 1D
+    if (row > 1 && col > 1) {
+        // Create 2D array computed grid
+        let matrix = [...Array(row)].map(e => Array());
+        for (var i = 0; i < col; i++) {
+            for (var j = 0; j < row; j++) {
+                if (innerEls[(row * i) + (j)]) {
+                    matrix[j].push(innerEls[(row * i) + (j)]);
+                } else {
+                    matrix[j].push(null);
+                }
+            }
+        }
+        // Calculate diagonal lines from matrix
+        let diags = [...Array(row + col - 1)].map(e => Array());
+        for (let line = 1; line <= (row + col - 1); line++) {
+            let start_col = Math.max(0, line - row);
+            let count = Math.min(line, (col - start_col), row);
+
+            for (let j = 0; j < count; j++) {
+                if (matrix[Math.min(row, line) - j - 1]) {
+                    diags[line - 1].push(matrix[Math.min(row, line) - j - 1][start_col + j])
+                }
+            }
+        }
+        // Apply increasing animation delay to each subsequent diagonal line
+        for (var i = 0; i < diags.length; i++) {
+            diags[i].forEach(el => {
+                el.style["-webkit-animation"] = "animation " + (1500 * (i + 1)) + "ms linear both";
+                el.style["animation"] = "animation " + (1500 * (i + 1)) + "ms linear both";
+            })
+        }
     }
 }
