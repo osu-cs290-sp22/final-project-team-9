c1b301880fafda8e60eba9712600fba4f58ba5cc
comac13 <97314421+comac13@users.noreply.github.com>
2022-06-07 16:11:46 -0700

    Revert "refresh"
    This reverts commit f7908b9c3102cb7361198c70f689a2c243127269.

--- final-project-team-9-main/api/routes/playback.js
+++
@@ -1,7 +0,0 @@
-const playbackRouter = require('express').Router();
-const playback = require("../controllers/playback.controller");
-
-playbackRouter.get("/play", playback.play);
-playbackRouter.get("/pause", playback.pause);
-
-module.exports = playbackRouter;
