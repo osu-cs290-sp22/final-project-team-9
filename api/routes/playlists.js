301e608c749b568c5fa9d17fa0987af078b33ab2
Carter Roeser <carter@cdgtech.one>
2022-06-08 00:48:20 -0700

    Overlay + Button Functionality

--- api/routes/playlists.js
+++ api/routes/playlists.js
@@ -4,6 +4,7 @@ const playlists = require("../controllers/playlists.controller");
 playlistRouter.get('/snapshot/:id', playlists.retrieveSnapshot);
 playlistRouter.get('/public/:id', playlists.retrievePublic);
 playlistRouter.get('/search', playlists.search);
+playlistRouter.delete('/graphs/delete/:id', playlists.deleteGraph);
 playlistRouter.get('/:id', playlists.getPlaylist);
 playlistRouter.get('/:id/metadata', playlists.getPlaylistMetadata);
 playlistRouter.post('/:id/share', playlists.share);
