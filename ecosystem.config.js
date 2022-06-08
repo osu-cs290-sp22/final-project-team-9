bece711769298ebedff7f9b071fce1353c5207f2
comac13 <97314421+comac13@users.noreply.github.com>
2022-06-07 17:07:29 -0700

    Revert "refreshed finally"
    This reverts commit 6532450b3836b2961c18e5589fc44e2f353f7549.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  apps : [{
-    script: 'express.js',
-    watch: '.',
-    name: 'smartlists',
-    instances: 0,
-    exec_mode: "cluster"
-  }],
+    apps: [{
+        script: 'server.js',
+        watch: '.',
+        name: 'smartlists',
+        instances: 0,
+        exec_mode: "cluster"
+    }],
 
-  deploy : {
-    production : {
-      ref  : 'origin/master',
-      repo : 'osu-cs290-sp22/final-project-team-9',
-      path : '.',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    deploy: {
+        production: {
+            ref: 'origin/master',
+            repo: 'osu-cs290-sp22/final-project-team-9',
+            path: '.',
+            'pre-deploy-local': '',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'pre-setup': ''
+        }
     }
-  }
 };
